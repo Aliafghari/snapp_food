688b9135dc0e7d35470072cabb5cf6857b6bc24f
Aliafghari <Aliafghari4@gmail.com>
2023-05-24 03:00:09 +0330

    Resources for week3

---
+++ app/Http/Requests/api/AddressRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests\api;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class AddressRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'title' => 'required|max:150',
+            'address' => 'required|max:255',
+            'latitude' => 'required',
+            'longitude' => 'required',
+        ];
+    }
+}
