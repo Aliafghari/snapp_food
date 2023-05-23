61c902ae2b303c90222d6e3cbf59055cc03c615a
Aliafghari <Aliafghari4@gmail.com>
2023-05-24 01:40:13 +0330

    foodCategory

---
+++ app/Http/Livewire/Admin/Modals/FoodCategoryEditModal.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Livewire\Admin\Modals;
+
+use App\Models\foodCategories;
+use Livewire\Component;
+
+class FoodCategoryEditModal extends Component
+{
+    public $foodCategory;
+    public $confirmingCategoryUpdate = false;
+
+    protected $listeners = ['EditModalConfirm'];
+
+    protected $rules = [
+        'foodCategory.FoodType' => 'required|min:3'
+    ];
+
+    public function EditModalConfirm(foodCategories $id)
+    {
+        $this->confirmingCategoryUpdate = true;
+        $this->foodCategory = $id;
+    }
+
+    public function editModal()
+    {
+        $this->validate();
+
+        if (isset($this->foodCategory->id)) {
+            $this->foodCategory->save();
+        }
+
+        $this->reset(['foodCategory']);
+        $this->emit('RefreshTable');
+        $this->emitTo('livewire-toast', 'show', " دسته بندی با موفقیت بروزرسانی شد :)");
+        $this->confirmingCategoryUpdate = false;
+    }
+
+    public function render()
+    {
+        return view('livewire.admin.modals.food-category-edit-modal');
+    }
+}
