3256a318271ebe556b69bf7e0eacdadd688c6992
Aliafghari <Aliafghari4@gmail.com>
2023-05-24 01:40:30 +0330

    Create FoodPartyAddModal.php

---
+++ app/Http/Livewire/Admin/Modals/FoodPartyAddModal.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Livewire\Admin\Modals;
+
+use App\Models\FoodParty;
+use Livewire\Component;
+
+class FoodPartyAddModal extends Component
+{
+    public $foodParty;
+    public $confirmFoodPartyAddModal = false;
+
+    protected $listeners = ['confirmFoodPartyAdd', 'EditModalConfirm' => 'confirmFoodPartyAdd'];
+
+    public function confirmFoodPartyAdd(FoodParty $id)
+    {
+        $this->foodParty = $id;
+        $this->confirmFoodPartyAddModal = true;
+    }
+
+    protected $rules = [
+        'foodParty.foodPartyName' => 'required|min:5',
+        'foodParty.Expire' => 'required|date_format:Y-m-d|after:today'
+    ];
+
+    public function addFoodPart()
+    {
+        $this->validate();
+
+        if ($this->foodParty->id) {
+
+            $this->foodParty->save();
+            $this->emitTo('livewire-toast', 'show', " فودپارتی با موفقیت ویرایش شد :) ");
+
+        } else
+            FoodParty::create([
+                'foodPartyName' => $this->foodParty->foodPartyName,
+                'ExpireTime' => $this->foodParty->Expire,
+                'status' => false
+            ]);
+
+        $this->emit('RefreshTable');
+        $this->emitTo('livewire-toast', 'show', " فودپارتی با موفقیت اضافه شد :) ");
+        $this->confirmFoodPartyAddModal = false;
+    }
+
+    public function render()
+    {
+        return view('livewire.admin.modals.food-party-add-modal');
+    }
+}
