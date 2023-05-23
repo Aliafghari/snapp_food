2ed4a396a87ea70e3eb05f48d0dfcdd0bebc84f7
Aliafghari <Aliafghari4@gmail.com>
2023-05-24 03:04:00 +0330

    Create OrderMailNotif.php

---
+++ app/Notifications/OrderMailNotif.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class OrderMailNotif extends Notification implements ShouldQueue
+{
+    use Queueable;
+
+    public $orders;
+    public $message;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct($order)
+    {
+        $this->orders = $order;
+        switch ($this->orders->OrderStatus) {
+            case 1:
+                $this->message = 'در حال بررسی';
+                break;
+            case 2:
+                $this->message = 'در حال آماده سازی';
+                break;
+            case 3:
+                $this->message = 'ارسال به مقصد';
+                break;
+            case 4:
+                $this->message = 'تحویل گرفته شد';
+        }
+
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param mixed $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param mixed $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+            ->line('قیمت کل : ' . $this->orders->Total_price)
+            ->line('وضعیت سفارش شما : ' . $this->message)
+            ->action('Notification Action', url('/'));
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param mixed $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
