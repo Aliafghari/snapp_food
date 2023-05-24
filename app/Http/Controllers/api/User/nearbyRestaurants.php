1f5e2c38164b24afbcd79c39d8384f5dc93e1453
Aliafghari <Aliafghari4@gmail.com>
2023-05-24 20:56:46 +0330

    Create nearbyRestaurants.php

---
+++ app/Http/Controllers/api/User/nearbyRestaurants.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers\api\User;
+
+use App\Http\Controllers\Controller;
+use App\Models\RestaurantDetail;
+use App\Models\UserAddress;
+use Illuminate\Support\Facades\DB;
+
+class nearbyRestaurants extends Controller
+{
+    public $lat;
+    public $long;
+    public $array = [];
+
+    public function __construct()
+    {
+        $this->middleware('auth:api');
+    }
+
+    public function index()
+    {
+        $query = UserAddress::where([['currentAddress', 1], ['user_id', auth('api')->user()->id]])->get()->first();
+        if ($query != null) {
+            $this->lat = $query->latitude;
+            $this->long = $query->longitude;
+            RestaurantDetail::all()->map(function ($value) {
+                $this->array[] = $value->select("*", DB::raw("6371 * acos(cos(radians(" . $this->lat . "))
+                                * cos(radians(latitude)) * cos(radians(longitude) - radians(" . $this->long . "))
+                                + sin(radians(" . $this->lat . ")) * sin(radians(latitude))) AS distance"))->orderBy('distance')->get();
+            });
+            unset($this->array[1]);
+            return $this->array;
+        } else {
+            return response()->json(['msg' => 'Please Set Current Location !']);
+        }
+    }
+
+    public function store()
+    {
+
+    }
+}
