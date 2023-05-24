291b9719714f79f592cb9c3d1bde1ea32cb2b6f5
Aliafghari <Aliafghari4@gmail.com>
2023-05-24 20:59:54 +0330

    Create BannerModal.php

---
+++ app/Http/Livewire/Admin/Modals/BannerModal.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Livewire\Admin\Modals;
+
+use Illuminate\Support\Facades\Storage;
+use Livewire\Component;
+use Livewire\WithFileUploads;
+
+class BannerModal extends Component
+{
+    use WithFileUploads;
+
+    public $photo = '';
+    public $confirmBannerAddModal = false;
+    protected $listeners = ['confirmBannerAdd'];
+
+    protected $rules = [
+        'photo' => 'image|mimes:jpg,png'
+    ];
+
+    public function confirmBannerAdd()
+    {
+        $this->confirmBannerAddModal = true;
+    }
+
+    public function AddBanner()
+    {
+        $this->validate();
+
+        $BannerName = str_replace(" ", "_", $this->photo->hashName());
+        $filename = $BannerName . '.' . $this->photo->getClientOriginalExtension();
+
+        if (!Storage::disk('public')->exists("photos/Banners/$filename")) {
+
+            \App\Models\Banner::create([
+                'name' => $BannerName,
+                'path' => "photos/Banners/$filename"
+            ]);
+
+            $this->photo->storeAs('photos/Banners', $filename);
+            $this->confirmBannerAddModal = false;
+            $this->emitTo('livewire-toast', 'show', 'ّبنر با موفقیت اضافه شد :)');
+        }
+
+        $this->emit('RefreshTable');
+        $this->confirmBannerAddModal = false;
+    }
+
+    public function render()
+    {
+        return view('livewire.admin.modals.banner-modal');
+    }
+}
