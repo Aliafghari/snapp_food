184d86ef70794a86412124d07f0d0fffd9c2fa74
Aliafghari <Aliafghari4@gmail.com>
2023-05-26 17:06:45 +0330

    Edit text

---
+++ resources/views/livewire/index-page.blade.php
@@ -0,0 +1,149 @@
+<div>
+    <div class="col-span-12 mt-8">
+        <br>
+        <br>
+        <h1>welcome</h1>
+    </div>
+</div>
+
+<div class="py-1 px-2 rounded-xl text-xs bg-cyan-400 text-white cursor-pointer font-medium">
+    <div class="col-span-12 mt-8">
+
+        <h2
+            class="text-lg font-medium truncate ml-5 mt-8 flex-1 w-full btn btn-outline-danger rounded-full mx-auto relative text-right bg-white">
+            {{-- <h2 class="py-1 px-2 rounded-full text-2xl bg-red-500 text-dark cursor-pointer font-medium"> --}}
+            <b>تبلیغات</b>
+
+        </h2>
+        {{-- <a href="" class="mr-auto flex items-center text-theme-26 dark:text-theme-33"> <i data-feather="refresh-ccw"
+                                                                                              class="w-4 h-4 ml-3"></i>
+                به روزرسانی داده </a> --}}
+
+        <div class="grid gap-6 mt-2">
+            <div class="mt-5 intro-x">
+                <div class=" w-full">
+                    <div class="tiny-slider" id="important-notes" style="transform: none">
+                        @if (!empty($Banners))
+                            @foreach ($Banners as $Banner)
+                                <div class="h-full rounded-md">
+                                    <img alt="Icewall Tailwind HTML Admin Template"
+                                        class="image-fit rounded-full h-full font-medium w-full flex items-center justify-center text-2xl"
+                                        src="{{ asset("storage/$Banner->path") }}">
+                                </div>
+                            @endforeach
+                        @endif
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+<div class="py-1 px-2 rounded-xl text-xs bg-cyan-400 text-white cursor-pointer font-medium">
+    <h2
+        class="text-lg font-medium truncate ml-5 mt-8 flex-1 w-full btn btn-outline-warning rounded-full mx-auto relative text-right bg-white">
+        {{-- <h2 class="py-1 px-2 rounded-full text-2xl bg-red-500 text-dark cursor-pointer font-medium"> --}}
+        <b>شعبه ها</b>
+    </h2>
+    <div class="grid grid-cols-12 gap-6 mt-5">
+        @if (!empty($Restaurant))
+            @foreach ($Restaurant as $item)
+                <div class="col-span-12 sm:col-span-6 xl:col-span-3 intro-y">
+                    <div class="report-box zoom-in">
+                        <div class="box p-5">
+                            <div class="flex">
+                                <i data-feather="shopping-cart" class="report-box__icon text-theme-21"></i>
+                                <div class="mr-auto">
+                                    <div class="report-box__indicator bg-theme-10 tooltip cursor-pointer"
+                                        title="تعداد فروش"> {{ $item->Orders->count() }} <i data-feather="chevron-up"
+                                            class="w-4 h-4 mr-0.5"></i>
+                                    </div>
+                                </div>
+                                <div class="mr-auto">
+                                    @if ($item->is_open)
+                                        <div class="report-box__indicator bg-green-500 tooltip cursor-pointer"
+                                            title="وضعیت فروشگاه">
+                                            باز است
+                                            <i data-feather="chevron-up" class="w-4 h-4 mr-0.5"></i>
+                                        </div>
+                                    @else
+                                        <div class="report-box__indicator bg-red-500 tooltip cursor-pointer"
+                                            title="وضعیت فروشگاه">
+                                            بسته است
+                                            <i data-feather="chevron-up" class="w-4 h-4 mr-0.5"></i>
+                                        </div>
+                                    @endif
+                                </div>
+                            </div>
+                            <div class="text-xl font-bold leading-8 mt-6 text-theme-17">
+                                <span class="text-sm text-gray-700">تعداد غذای موجود :</span>
+                                {{ $item->food->count() }}
+                            </div>
+                            <div class="text-xl font-bold leading-8 mt-6 text-theme-17">
+                                <span class="text-sm text-gray-700">نام رستوران :</span>
+                                {{ $item->name }}
+                            </div>
+                            <div class="text-xl font-bold leading-8 mt-6 text-theme-17">
+                                @php
+                                    $filename = str_replace(' ', '_', $item->name);
+                                @endphp
+                                <img src="{{ asset("storage/photos/Restaurant/$filename" . '.jpg') }}"
+                                    alt="RestaurantBaner" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        @endif
+    </div>
+    <br>
+    <br>
+</div>
+
+
+
+<div class="py-1 px-2 rounded-xl text-xs bg-cyan-400 text-white cursor-pointer font-medium">
+    <h2
+        class="text-lg font-medium truncate ml-5 mt-8 flex-1 w-full btn btn-outline-success rounded-full mx-auto relative text-right bg-white">
+        <b>فودپارتی</b>
+    </h2>
+    <div class="grid grid-cols-4 gap-6 mt-2">
+        <div class="mt-5 intro-x">
+            <div class="box zoom-in w-full">
+                <div class="tiny-slider" id="important-notes">
+                    @if (!empty($FoodParty))
+                        @foreach ($FoodParty[0] as $food)
+                            @if ($food->first() != null)
+                                <div class="p-5">
+                                    @php
+                                        $filename = str_replace(' ', '_', $food->first()->title);
+                                    @endphp
+                                    <img alt="Icewall Tailwind HTML Admin Template" class="image-fit w-full"
+                                        src="{{ asset("/storage/photos/Foods/$filename" . '_1.jpg') }}">
+                                    <div class="text-base font-medium truncate mt-2">{{ $food->first()->title }} </div>
+                                    <div class="text-gray-500 mt-1">{{ $food->first()->price }}</div>
+                                    <div class="text-gray-600 text-right mt-1">{{ $food->first()->raw_material }}</div>
+                                    <div class="font-medium flex mt-5">
+                                        <button type="button" class="btn btn-outline-secondary py-1 px-2 ">خرید
+                                        </button>
+                                        <button type="button" class="btn btn-secondary py-1 px-2 ml-auto ml-auto">
+                                            @foreach (DB::table('food_partiey')->get() as $item)
+                                                @if ($item->food_id == $food->id)
+                                                    {{ $item->DiscountAmount }}% تخفیف
+                                                @endif
+                                            @endforeach
+                                        </button>
+
+                                    </div>
+                                </div>
+                            @endif
+                        @endforeach
+                    @endif
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ resources/views/navigation-menu.blade.php
@@ -0,0 +1,225 @@
+<div>
+    <nav class="navbar navbar-dark bg-dark fixed-top mb-5">
+        <div class="container-fluid">
+            <a href="{{ route('/') }}" class="-intro-x hidden md:flex">
+                <img alt="#" class="w-6" src="dist/images/1558260216873.jfif">
+                <span class="text-white text-lg mr-3 adjust "> اسنپ <span class="font-medium">فود</span> </span>
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas"
+                data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="offcanvas offcanvas-end text-bg-tertiary" tabindex="-1" id="offcanvasDarkNavbar"
+                aria-labelledby="offcanvasDarkNavbarLabel">
+                <div class="offcanvas-header">
+                    <h5 class="offcanvas-title font-bold" id="offcanvasDarkNavbarLabel">منو</h5>
+                    <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas"
+                        aria-label="Close"></button>
+                </div>
+                <div class="offcanvas-body">
+                    <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+                        <!-- BEGIN: Breadcrumb -->
+                        <div class="-intro-x breadcrumb ml-auto"><a class="breadcrumb--active"></a></div>
+                        <!-- END: Breadcrumb -->
+                        <li class="nav-item dropdown">
+                            <div class="intro-x dropdown w-8 h-8 mb-4">
+                                <div class="dropdown-toggle w-8 h-8 rounded-full overflow-hidden shadow-lg image-fit zoom-in scale-110 "
+                                    role="button" aria-expanded="false">
+                                    @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
+                                        <img alt="#" src="dist/images/user.png">
+                                    @else
+                                        <img alt="#" src="dist/images/profile-19.jpg">
+                                    @endif
+                                </div>
+                                <div class="dropdown-menu w-56">
+                                    <div class="dropdown-menu__content box bg-theme-11 dark:bg-dark-6 text-white">
+                                        <div class="p-4 border-b border-theme-12 dark:border-dark-3">
+                                            <div class="font-medium">{{ Auth::user()->name }}</div>
+                                            <div class="text-xs text-theme-13 mt-0.5 dark:text-gray-600">
+                                                {{ Auth::user()->email }}
+                                            </div>
+                                        </div>
+                                        <div class="p-2">
+                                            <a href="{{ route('profile.show') }}"
+                                                class="flex items-center block p-2 transition duration-300 ease-in-out hover:bg-theme-1 dark:hover:bg-dark-3 rounded-md">
+                                                <i data-feather="user" class="w-4 h-4 ml-2"></i> پروفایل </a>
+                                            @if (Laravel\Jetstream\Jetstream::hasApiFeatures())
+                                                <a href="{{ route('api-tokens.index') }}"
+                                                    class="flex items-center block p-2 transition duration-300 ease-in-out hover:bg-theme-1 dark:hover:bg-dark-3 rounded-md">
+                                                    <i data-feather="edit" class="w-4 h-4 ml-2"></i>
+                                                    {{ __('توکن شما') }} </a>
+                                            @endif
+                                            <a href="{{ route('profile.show') }}"
+                                                class="flex items-center block p-2 transition duration-300 ease-in-out hover:bg-theme-1 dark:hover:bg-dark-3 rounded-md">
+                                                <i data-feather="lock" class="w-4 h-4 ml-2"></i>بازیابی رمزعبور</a>
+                                        </div>
+                                        <div class="p-2 border-t border-theme-12 dark:border-dark-3">
+                                            <form method="POST" action="{{ route('logout') }}" x-data>
+                                                @csrf
+
+                                                <a href="{{ route('logout') }}"
+                                                    class="flex items-center block p-2 transition duration-300 ease-in-out hover:bg-theme-1 dark:hover:bg-dark-3 rounded-md"
+                                                    @click.prevent="$root.submit();">
+                                                    <i data-feather="toggle-right" class="w-4 h-4 ml-2"></i>
+                                                    {{ __('خروج') }}
+                                                </a>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+                            <br>
+                        </li>
+                        @role('superadmin')
+                            <li>
+                                <x-jet-nav-link href="{{ route('Admin') }}" :active="request()->routeIs('Admin')" class="top-menu">
+
+                                    <div class="top-menu__title">داشبورد</div>
+                                </x-jet-nav-link>
+                            </li>
+                            <br>
+                            <li>
+                                <x-jet-nav-link href="{{ route('restaurantCategory') }}" :active="request()->routeIs('restaurantCategory')"
+                                    class="top-menu">
+
+                                    <div class="top-menu__title">نوع رستوران</div>
+                                </x-jet-nav-link>
+                            </li>
+                            <br>
+                            <li>
+                                <x-jet-nav-link href="{{ route('foodCategory') }}" :active="request()->routeIs('foodCategory')" class="top-menu">
+
+                                    <div class="top-menu__title"> غذا ها</div>
+                                </x-jet-nav-link>
+                            </li>
+                            <br>
+                            {{--            <li> --}}
+                            {{--                <x-jet-nav-link href="{{ route('roles') }}" :active="request()->routeIs('roles')" class="top-menu"> --}}
+                            {{--                    <div class="top-menu__icon"><i data-feather="settings" class="block mx-auto"></i></div> --}}
+                            {{--                    <div class="top-menu__title"> تنظیمات دسترسی ها</div> --}}
+                            {{--                </x-jet-nav-link> --}}
+                            {{--            </li>
+                                <br> --}}
+                            <li>
+                                <x-jet-nav-link href="{{ route('DiscountPanel') }}" :active="request()->routeIs('DiscountPanel')" class="top-menu">
+
+                                    <div class="top-menu__title"> تخفیف ها</div>
+                                </x-jet-nav-link>
+                            </li>
+                            <br>
+                            <li>
+                                <x-jet-nav-link href="{{ route('FoodParty') }}" :active="request()->routeIs('FoodParty')" class="top-menu">
+
+                                    <div class="top-menu__title">فود پارتی</div>
+                                </x-jet-nav-link>
+                            </li>
+                            <br>
+                            <li>
+                                <x-jet-nav-link href="{{ route('FrontBanner') }}" :active="request()->routeIs('FrontBanner')" class="top-menu">
+
+                                    <div class="top-menu__title"> بنر ها</div>
+                                </x-jet-nav-link>
+                            </li>
+                            <br>
+
+                            <li>
+                                <x-jet-nav-link href="{{ route('AllUsers') }}" :active="request()->routeIs('AllUsers')" class="top-menu">
+
+                                    <div class="top-menu__title">کاربران</div>
+                                </x-jet-nav-link>
+                            </li>
+                            <br>
+                            <li>
+                                <x-jet-nav-link href="{{ route('ReportCharts') }}" :active="request()->routeIs('ReportCharts')" class="top-menu">
+
+                                    <div class="top-menu__title">گزارش ها و نمودارها</div>
+                                </x-jet-nav-link>
+                            </li>
+                            <br>
+                            <li>
+                                <x-jet-nav-link href="{{ route('CommentManagment') }}" :active="request()->routeIs('CommentManagment')" class="top-menu">
+
+                                    <div class="top-menu__title">مدیریت کامنت ها</div>
+                                </x-jet-nav-link>
+                            </li>
+                            <br>
+                            <li>
+                                <x-jet-nav-link href="{{ route('AllComments') }}" :active="request()->routeIs('AllComments')" class="top-menu">
+
+                                    <div class="top-menu__title">صفحه چت</div>
+                                </x-jet-nav-link>
+                            </li>
+                            <br>
+                            <li>
+                                <x-jet-nav-link href="{{ route('DeletedComment') }}" :active="request()->routeIs('DeletedComment')" class="top-menu">
+
+                                    <div class="top-menu__title">کامنت های حذف شده</div>
+                                </x-jet-nav-link>
+                            </li>
+                            <br>
+                        @endrole
+                        @role('seller')
+                            <li>
+                                <x-jet-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')" class="top-menu">
+                                    {{-- <div class="top-menu__icon"><i data-feather="home"></i></div> --}}
+                                    <div class="top-menu__title">داشبورد</div>
+                                </x-jet-nav-link>
+                            </li>
+                            <br>
+                            <li>
+                                <x-jet-nav-link href="{{ route('RestaurantPanel') }}" :active="request()->routeIs('RestaurantPanel')" class="top-menu">
+                                    {{-- <div class="top-menu__icon">
+                                            <i data-feather="aperture" class="block mx-auto"></i>
+                                        </div> --}}
+                                    <div class="top-menu__title"> پروفایل رستوران</div>
+                                </x-jet-nav-link>
+                            </li>
+                            <br>
+                            <li>
+                                <x-jet-nav-link href="{{ route('FoodPanel') }}" :active="request()->routeIs('FoodPanel')" class="top-menu">
+                                    {{-- <div class="top-menu__icon"><i data-feather="coffee" class="block mx-auto"></i></div> --}}
+                                    <div class="top-menu__title"> تنظیمات غذاها</div>
+                                </x-jet-nav-link>
+                            </li>
+                            <br>
+                            <li>
+                                <x-jet-nav-link href="{{ route('OrdersPanel') }}" :active="request()->routeIs('OrdersPanel')" class="top-menu">
+                                    {{-- <div class="top-menu__icon"><i data-feather="shopping-cart" class="block mx-auto"></i></div> --}}
+                                    <div class="top-menu__title"> سفارش ها</div>
+                                </x-jet-nav-link>
+                            </li>
+                            <br>
+                            <li>
+                                <x-jet-nav-link href="{{ route('ArchiveOrder') }}" :active="request()->routeIs('ArchiveOrder')" class="top-menu">
+                                    {{-- <div class="top-menu__icon"><i data-feather="shopping-cart" class="block mx-auto"></i></div> --}}
+                                    <div class="top-menu__title"> آرشیو سفارشات</div>
+                                </x-jet-nav-link>
+                            </li>
+                            <br>
+                            <li>
+                                <x-jet-nav-link href="{{ route('CommentsPanel') }}" :active="request()->routeIs('CommentsPanel')" class="top-menu">
+                                    {{-- <div class="top-menu__icon"><i data-feather="inbox" class="block mx-auto"></i></div> --}}
+
+                                    <div class="top-menu__title"> نظر ها</div>
+                                </x-jet-nav-link>
+                            </li>
+                            <br>
+                        @endrole
+                    </ul>
+                    <div class="intro-x relative ml-3 sm:ml-6">
+                        <div class="search hidden sm:block">
+                            <input type="text"
+                                class="search__input form-control dark:bg-dark-1 border-transparent placeholder-theme-8"
+                                placeholder="جستجو...">
+                            <i data-feather="search" class="search__icon dark:text-gray-300"></i>
+                        </div>
+                        <a class="notification sm:hidden" href=""> <i data-feather="search"
+                                class="notification__icon dark:text-gray-300"></i> </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </nav>
