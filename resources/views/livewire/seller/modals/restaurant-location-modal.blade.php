2067c25197a2754a879180bb67dcda40602c6dd7
Aliafghari <Aliafghari4@gmail.com>
2023-06-05 09:27:08 +0330

    Update restaurant-location-modal.blade.php

--- resources/views/livewire/seller/modals/restaurant-location-modal.blade.php
+++ resources/views/livewire/seller/modals/restaurant-location-modal.blade.php
@@ -3,11 +3,21 @@
         <x-slot name="title">
             <h2 class="font-medium text-base ml-auto">افزودن غذای به فودپارتی</h2>
         </x-slot>
-
         <x-slot name="content">
             <div class="col-span-12 sm:col-span-12" wire:ignore>
                 <div class="mb-6">
                     <div class="mt-3">
+
+
+                        <label for="Location" class="form-label">لوکیشن رستوران</label>
+
+
+                        <x-jet-input-error for="lat" class="mt-2" />
+                        <x-maps-leaflet id="map" :center-point="['lat' => 35.701253490910126, 'long' => 51.34916022406515]" :zoom-level="18" :markers="[['lat' => 35.701253490910126, 'long' => 51.34916022406515]]"
+                            wire:model.click="$emit('saveLocation')"
+                            style="width: 600px; height: 450px; background: #eee; border: 2px solid #aaa;">
+                        </x-maps-leaflet>
+
 
                     </div>
                 </div>
@@ -29,7 +39,7 @@
 
         <x-slot name="footer">
             <x-jet-secondary-button wire:click="$set('confirmRestaurantLocationModal', false)"
-
+                class="btn btn-outline-secondary w-20 ml-1">
                 {{ __('لغو') }}
             </x-jet-secondary-button>
 
